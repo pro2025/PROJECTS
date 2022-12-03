9f6218cd1030461f036e16e8d16f4a08250c2ab4
T Nikhitha <119213327+pro2025@users.noreply.github.com>
2022-12-03 17:27:39 +0530

    Add files via upload

---
+++ 1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,z;
+clrscr();
+printf("Enter the value of x:\n");
+scanf("%d",&x);
+printf("Enter the value of y:\n");
+scanf("%d",&y);
+z=x+y;
+printf("Sum of x and y is:%d",z);
+getch();
+}
