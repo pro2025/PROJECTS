8012036f3e326d36f1961bb34635e3870aa15c45
T Nikhitha <119213327+pro2025@users.noreply.github.com>
2022-12-15 00:13:41 +0530

    Code to swap two numbers

---
+++ SWAP.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+clrscr();
+printf("Enter the value of a:");
+scanf("%d",&a);
+printf("Enter the vlaue of b:");
+scanf("%d",&b);
+printf("The number is:%d\n",b);
+printf("Another number is:%d",a);
+getch();
+}
