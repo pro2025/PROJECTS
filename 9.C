e9ef0c285e3d4038327716f4bd13b5cd25ef1e3a
T Nikhitha <119213327+pro2025@users.noreply.github.com>
2022-12-27 15:43:47 +0530

    Code to compute the smallest number of notes that will combine to give the real amount

--- 9.C
+++ 9.C
@@ -2,25 +2,26 @@
 #include<conio.h>
 void main()
 {
-int a,b,c;
+int amount,a;
 clrscr();
-printf("Enter the value of side AB:");
-scanf("%d",&a);
-printf("Enter the value of side BC:");
-scanf("%d",&b);
-printf("Enter the value of side CA:");
-scanf("%d",&c);
-if (a==b && b==c)
-{
-printf("Triangle is Equilateral");
-}
-else if(a==b||b==c||c==a)
-{
-printf("Triangle is Isosceles");
-}
-else
-{
-printf("Triangle is scalene");
-}
+printf("Enter the amount:");
+scanf("%d",&amount);
+a=amount/100;
+amount=amount-(a*100);
+printf("%d x100=%d\n",a,(a*100));
+a=amount/50;
+amount=amount-(a*50);
+printf("%d x50=%d\n",a,(a*50));
+a=amount/10;
+amount=amount-(a*10);
+printf("%d x10=%d\n",a,(a*10));
+a=amount/5;
+amount=amount-(a*5);
+printf("%d x5=%d\n",a,(a*5));
+a=amount/2;
+amount=amount-(a*2);
+printf("%d x2=%d\n",a,(a*2));
+a=amount/1;
+printf("%d x1=%d\n",a,(a*1));
 getch();
 }
